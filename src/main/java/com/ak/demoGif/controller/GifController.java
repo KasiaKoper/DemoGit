e653891684337400b9a183b033633d27b720407f
KasiaKoper <katarzynakoper2@gmail.com>
2019-05-11 17:14:44 +0200

    git added

--- src/main/java/com/ak/demoGif/controller/GifController.java
+++ src/main/java/com/ak/demoGif/controller/GifController.java
@@ -1,4 +1,22 @@
 package com.ak.demoGif.controller;
 
+import com.ak.demoGif.model.Gif;
+import com.ak.demoGif.model.repository.GifRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@Controller
 public class GifController {
+
+    @RequestMapping("/showGifsInBrowser")
+    @ResponseBody
+
+    public String showGifsInBrowser(){
+        return "gifs";
+    }
+
 }
--- src/main/java/com/ak/demoGif/model/repository/GifRepository.java
+++ src/main/java/com/ak/demoGif/model/repository/GifRepository.java
@@ -1,4 +1,17 @@
 package com.ak.demoGif.model.repository;
 
+import com.ak.demoGif.model.Gif;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class GifRepository {
+
+    private static List<Gif> ALL_GIFS = Arrays.asList(new Gif("android-explosion", "mols", true),
+            new Gif("ben-and-mike", "mika", true),
+            new Gif("book-dominos", "mem", false),
+            new Gif("compiler-bot", "bot", true),
+            new Gif("cowboy-coder", "code", false),
+            new Gif("infinite-andrew", "andrew", true));
+
 }

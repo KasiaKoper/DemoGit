0573c332ebad692f021a6e9b9b6951cf26b04f42
KasiaKoper <katarzynakoper2@gmail.com>
2019-05-11 17:43:50 +0200

    git added

--- src/main/java/com/ak/demoGif/controller/GifController.java
+++ src/main/java/com/ak/demoGif/controller/GifController.java
@@ -12,11 +12,16 @@ import java.util.List;
 @Controller
 public class GifController {
 
+    //@Autowired          // to przywiązuje nam klasę do klasy GifRepository, gdzie mam metody
+            // dzieki temu mozemy korzystac z tej metody (nawet jesli nie jest public static)
+            // czyli to jest alternatywa, do tworzenia nowego obiektu aby wywolac na nim metode
+
+            GifRepository gifRepository;
+
     @RequestMapping("/showGifsInBrowser")
     @ResponseBody
-
-    public String showGifsInBrowser(){
-        return "gifs";
+    public String showGifsInBrowser() {
+        return gifRepository.getGifsNames();
     }
 
 }
--- src/main/java/com/ak/demoGif/model/repository/GifRepository.java
+++ src/main/java/com/ak/demoGif/model/repository/GifRepository.java
@@ -1,10 +1,13 @@
 package com.ak.demoGif.model.repository;
 
 import com.ak.demoGif.model.Gif;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Repository         //gdy dodajemy to mamy tzw BINa, związaliśmy klasę, Spring będzie nam tworzył sam obiekty tej klasy
 public class GifRepository {
 
     private static List<Gif> ALL_GIFS = Arrays.asList(new Gif("android-explosion", "mols", true),
@@ -14,4 +17,20 @@ public class GifRepository {
             new Gif("cowboy-coder", "code", false),
             new Gif("infinite-andrew", "andrew", true));
 
+    public String getGifsNames() {
+        String result="";
+
+        for (Gif allGif : ALL_GIFS) {
+            result+=allGif.getName();
+        }
+        return result;
+    }
+
+    public static List<Gif> getAllGifs() {
+        return ALL_GIFS;
+    }
+
+    public static void setAllGifs(List<Gif> allGifs) {
+        ALL_GIFS = allGifs;
+    }
 }
